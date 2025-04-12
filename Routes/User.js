9f405aca3841fe1070deccbc0b94ada7ebcd885f
skytechrick <ricksarkar2005@gmail.com>
2025-04-12 16:51:08 +0530

    feat: join and get all users group

--- Routes/User.js
+++ Routes/User.js
@@ -541,4 +541,5 @@ const checkUserMiddleware = async (req, res, next) => {
 
 User.post("/group/create", checkUserMiddleware , group.createGroup);
 User.post("/group/all", checkUserMiddleware , group.getAllGroups);
-
+User.post("/group/users", checkUserMiddleware , group.getAllUsersGroups);
+User.post("/group/join", checkUserMiddleware , group.joinAGroup);
--- controller/group.js
+++ controller/group.js
@@ -82,7 +82,131 @@ const getAllGroups = async (req, res) => {
     }
 }
 
+const getAllUsersGroups = async (req, res) => {
+    try {
+        const user = req.user;
+        const groups = await Model.group.find({status: "active"}).populate("userId").exec();
+        
+
+        const members = groups.filter((group) => {
+            const allMembers = group.members;
+            const isMember = allMembers.includes(user._id);
+            return isMember;
+        }
+        );
+        if (!members) {
+            return res.status(404).json({
+                status: "error",
+                message: "No groups found"
+            });
+        }
+        
+        if (!groups) {
+            return res.status(404).json({
+                status: "error",
+                message: "No groups found"
+            });
+        }
+        res.status(200).json({
+            status: "success",
+            message: "Groups fetched successfully",
+            groups
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            status: "error",
+            message: "Internal server error"
+        });
+    }
+}
+
+
+const joinAGroup = async (req, res) => {
+    try {
+
+        const user = req.user;
+        const { groupId } = req.body;
+        if (!groupId) {
+            return res.status(400).json({
+                status: "error",
+                message: "Please provide groupId"
+            });
+        }
+
+        const isExist = await Model.group.findOne({
+            _id: groupId,
+            status: "waiting",
+        });
+
+        if (!isExist) {
+            return res.status(404).json({
+                status: "error",
+                message: "Group with status waiting not found"
+            });
+        }
+
+        const allMembers = isExist.members;
+        
+        const maximumMember = isExist.maximumMember;
+        const currentMember = allMembers.length;
+        if (currentMember >= maximumMember) {
+            return res.status(400).json({
+                status: "error",
+                message: "Group is full"
+            });
+        }
+
+        const isAlreadyMember = allMembers.includes(user._id);
+
+        if (isAlreadyMember) {
+            return res.status(400).json({
+                status: "error",
+                message: "You are already a member of this group"
+            });
+        }
+
+        const stakeAmount = isExist.stakeAmount;
+        const userBalance = user.Tokens_Earned;
+
+        if (userBalance < stakeAmount) {
+            return res.status(400).json({
+                status: "error",
+                message: "You don't have enough balance to join this group"
+            });
+        }
+
+        allMembers.push(user._id);
+
+        const updatedGroup = await Model.group.findByIdAndUpdate(
+            groupId,
+            {
+                members: allMembers,
+            },
+            { new: true }
+        );
+
+        user.Tokens_Earned = userBalance - stakeAmount;
+        user.groups.push(groupId);
+        await user.save();
+        res.status(200).json({
+            status: "success",
+            message: "Group joined successfully",
+            group: updatedGroup
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            status: "error",
+            message: "Internal server error"
+        });
+    }
+};
+
 module.exports = {
     createGroup,
-    getAllGroups
+    getAllGroups,
+    getAllUsersGroups,
+    joinAGroup,
 }
