5d9baf7c14e03c3d9a609e13197ead675a073abc
skytechrick <ricksarkar2005@gmail.com>
2025-04-12 10:33:21 +0530

    first commit

---
+++ Models.js
@@ -0,0 +1,84 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
+const URL = `mongodb+srv://${process.env.Mongodb_Username}:${process.env.Mongodb_Password}@${process.env.Mongodb_Domain}/${process.env.Mongodb_DataBase}`;
+
+mongoose.connect(URL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to database');
+});
+const Schema = mongoose.Schema;
+const Model = mongoose.model;
+
+
+
+
+
+
+
+
+const userSchema = new Schema({
+    _id:{
+        type: String,
+        required: true
+    },
+    Email:{
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    Name:{
+        type: String,
+        default: "",
+    },
+    Profile:{
+        type: String,
+        default: "",
+    },
+    Authentication:{
+        Token:{
+            type: String,
+        },
+        Date:{
+            type: Date,
+            default: Date()
+        }
+    },
+    Tokens_Earned:{
+        type: Number,
+        required: true
+    },
+    Journals:[
+        {
+            _id : false,
+            ID:{
+                type: String,
+                required: true
+            },
+            Title:{
+                type: String,
+                required: true
+            },
+            Description:{
+                type: String,
+                required: true
+            },
+            Date:{
+                type: Date,
+                default: Date()
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date()
+    },
+});
+
+const Users = Model('Users', userSchema);
+
+module.exports = {
+    Users:Users
+};
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const Users = require("./Routes/User.js");
+
+const app = express();
+dotenv.config();
+const Port = process.env.Port;
+// 'http://localhost:3000', 'http://localhost:5173',
+const corsOptions = {
+    origin: ['https://wellnotes1-00.vercel.app', 'http://localhost:5173', 'https://zipbuy.in'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+};
+
+
+app.use(cors(corsOptions));
+app.use('/img', express.static('./Images/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/api", Users);
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        Status: "Success",
+        Message: "Server is running",
+    });
+});
+
+app.listen(Port, () => {
+    console.log(`Server is running on port ${Port}`);
+});
